fd0d65e61e72118d422ad726ec7044e9ba4c4cd4
syl8075 <syl8075@nyu.edu>
2026-05-04 22:15:12 +0800

    lkjhgf

--- Project_B/sketch.js
+++ Project_B/sketch.js
@@ -29,11 +29,13 @@ function draw() {
   background(220, 10);
 
 
-  if (isActive) {
-    b.activate();
-  } else {
+  if (isActive === false) {
     textAlign(CENTER);
-    text("Type something and press ENTER", width / 2, height / 2);
+
+    fill(0);
+    text(myInput.value(), width / 2, height / 2);
+  } else {
+    b.activate();
   }
 }
 
@@ -43,8 +45,14 @@ function keyPressed() {
     startAnimation();
   }
 
-  if (keyCode === ' ') {
-    b.triggerStage3();
+  if (key === " ") {
+    if (b.stage === 2) {
+      b.stage = 3;
+      b.yUK1 = b.y;
+      b.expandRight = 0;
+      b.expandLeft = 0;
+    }
+
   }
 }
 
@@ -55,17 +63,64 @@ function startAnimation() {
 
 class textBox {
   constructor(startX, startY) {
+    //Part one
     this.x = startX;
     this.y = startY;
 
     this.yU = 250;
-    this.yUK1 = 0;
+    this.yUK = 0;
+    this.yUKP = 250;
+    this.yUK1 = 250;
 
     this.speedY = random(1, 3);
     this.stage = 0;
-    this.textFade = 0;
+    this.textFade = 10;
 
-    this.expandLeft = .5;
+    //Part 2
+    this.expandRight = 0;
+    this.expandLeft = 0;
+
+    this.spreadRight = 0;
+    this.spreadLeft = 0;
+
+    //Part 3
+    this.targetSpread = 60;
+    this.easing = 0.08;
+
+    this.particles = [];
+    this.centerX = this.x;
+    this.centerY = this.y;
+    this.radius = 120;
+
+    this.stage4Fade = 0;
+    this.transitionPause = 0;
+
+    //Part 4
+    this.final = 0;
+    this.gatherStarted = false;
+
+    this.stageTimer = 0;
+
+    //Part 5
+    this.splitText = ["ㅁ", "ㅜ", "ㅇ", "ㅓ", "ㅅ", " ", "ㅇ", "ㅣ", "ㄹ", "ㄲ", "ㅏ", "ㅇ", "ㅛ"];
+    this.breakApart = false;
+    this.splitX = [];
+    this.splitY = [];
+    this.splitSpeed = [];
+  }
+  displayGlitch() {
+    let txt = myInput.value();
+    let result = "";
+
+    for (let i = 0; i < txt.length; i++) {
+      if (random() < 0.1) {
+        result += chars[floor(random(chars.length))];
+      } else {
+        result += txt[i];
+      }
+    }
+
+    text(result, this.x, this.y);
   }
   display0() {
 
@@ -75,8 +130,6 @@ class textBox {
     this.stage = 1;
     this.yU = this.y;
     this.yUK1 = 0;
-
-
   }
   activate() {
     if (this.stage === 1) {
@@ -91,14 +144,34 @@ class textBox {
       this.update2();
       this.display2();
 
-      if (this.yUK1 > this.y) {
-        this.yUK1 = this.y;
-        text("Try pressing space", this.x, this.y + 300);
+      if (this.yUK >= this.y) {
+        this.yUK = this.y;
       }
     }
     else if (this.stage === 3) {
       this.update3();
       this.display3();
+
+      if (this.expandRight > 150) {
+        this.transitionPause++;
+
+        if (this.transitionPause > 30) {
+          this.stage = 4;
+          this.transitionPause = 0;
+        }
+      }
+    }
+    else if (this.stage === 4) {
+      if (!this.gatherStarted) {
+        this.initStage4();
+        this.gatherStarted = true;
+      }
+      this.update4();
+      this.display4();
+    }
+    else if (this.stage === 5) {
+      this.update5();
+      this.display5();
     }
   }
   update1() {
@@ -107,60 +180,177 @@ class textBox {
   display1() {
     textAlign(CENTER);
     fill(0);
-    text(myInput.value(), this.x, this.y);
+    this.displayGlitch();
 
-    for (let i = -40; i <= 40; i += 10) {
+    for (let i = -30; i <= 30; i += 15) {
       fill(0);
-      text(chars[floor(random(chars.length))], this.x + i + this.expandLeft, this.yU + random(-50, 0));
+      text(chars[floor(random(chars.length))], this.x + i + this.spreadRight, this.yU + random(-50, 0));
+      text(chars[floor(random(chars.length))], this.x + i + this.spreadLeft, this.yU + random(-50, 0));
     }
 
-    this.expandLeft = this.expandLeft - random(-2, 1);
+    this.spreadRight += (20 - this.spreadRight) * 0.1;
+    this.spreadLeft += (-20 - this.spreadLeft) * 0.1;
   }
   update2() {
-    this.yUK1 += this.speedY;
+    this.yUK += this.speedY;
   }
-
   display2() {
     textAlign(CENTER);
 
-    for (let i = -40; i <= 40; i += 10) {
-      fill(0);
+    fill(0, 50);
+    text(myInput.value(), this.x, this.y);
+
+    if (this.yUK >= 200) {
+      this.spreadRight += (this.targetSpread - this.spreadRight) * this.easing;
+      this.spreadLeft += (-this.targetSpread - this.spreadLeft) * this.easing;
+    }
+    fill(0, 160);
+    for (let i = -20; i <= 20; i += 10) {
       text(
         charsK[floor(random(charsK.length))],
         this.x + i,
-        this.yUK1 + random(-50, 0)
+        this.yUK + random(-5, 5)
       );
     }
-  } triggerStage3() {
-    if (this.stage === 2) {
-      this.stage = 3;
+    fill(0, 120);
+    for (let i = -30; i <= 30; i += 15) {
+      text(
+        charsK[floor(random(charsK.length))],
+        this.x + i + this.spreadRight * 0.5,
+        this.yUK + random(-10, 10)
+      );
+
+      text(
+        charsK[floor(random(charsK.length))],
+        this.x + i + this.spreadLeft * 0.5,
+        this.yUK + random(-10, 10)
+      );
+    }
+    fill(0, 80);
+    for (let i = -30; i <= 30; i += 15) {
+      text(
+        charsK[floor(random(charsK.length))],
+        this.x + i + this.spreadRight,
+        this.yUK + random(-15, 15)
+      );
+
+      text(
+        charsK[floor(random(charsK.length))],
+        this.x + i + this.spreadLeft,
+        this.yUK + random(-15, 15)
+      );
+    }
+    if (this.yUK >= this.y) {
+      fill(0, 150);
+      text("Press SPACE", this.x, this.y + 300);
     }
   }
   update3() {
-    this.yUK1 += this.speedY * 2; // faster fall
+    this.expandRight += 2;
+    this.expandLeft -= 2;
   }
-  display3() {
+  display3() {// also not workiing try to fix it
     textAlign(CENTER);
 
     for (let i = -80; i <= 80; i += 10) {
-      text(
-        charsK[floor(random(charsK.length))],
-        this.x + i,
-        this.yUK1 + random(-100, 100)
-      );
+      text(charsK[floor(random(charsK.length))], this.x + i + this.expandRight, this.yUK1 + random(-60, 20));
+      text(charsK[floor(random(charsK.length))], this.x + i + this.expandLeft, this.yUK1 + random(-60, 20));
+    }
+
+    this.expandRight = this.expandRight - random(-2, 1);
+    this.expandLeft = this.expandLeft + random(-2, 1);
+  } initStage4() {
+    this.particles = [];
+
+    for (let i = 0; i < 80; i++) {
+
+      let p = {};
+
+      p.x = this.x + random(-200, 200);
+      p.y = this.y + random(-200, 200);
+
+      let angle = random(TWO_PI);
+
+      p.targetX = this.x + cos(angle) * 100;
+      p.targetY = this.y + sin(angle) * 100;
+
+      p.char = random(charsK);
+
+      this.particles.push(p);
     }
   }
-}
+  update4() {
+    this.stage4Fade += 5;
+    for (let i = 0; i < this.particles.length; i++) {
+
+      let currentX = this.particles[i].x;
+      let targetX = this.particles[i].targetX;
+
+      let currentY = this.particles[i].y;
+      let targetY = this.particles[i].targetY;
+
+      this.particles[i].x = currentX + (targetX - currentX) * 0.05;
+      this.particles[i].y = currentY + (targetY - currentY) * 0.05;
+    }
+    this.stageTimer++;
+
+    if (this.stageTimer > 180) {
+      this.stage = 5;
+      this.stageTimer = 0;
+    }
+  }
+  display4() {
+    textAlign(CENTER);
+    fill(0, this.stage4Fade);
+
+    for (let p of this.particles) {
+      fill(0, 120);
+      text(p.char, p.x, p.y);
+
+      fill(0, 50);
+      text(p.char, p.x + random(-2, 2), p.y + random(-2, 2));
+    }
+  }
+  update5() {
+    this.final += 2;
+    this.stageTimer++;
+
+    if (this.stageTimer > 180 && !this.breakApart) {
+      this.breakApart = true;
+
+      this.final = 255;
+
+      for (let i = 0; i < this.splitText.length; i++) {
+        this.splitX[i] = this.x + i * 22 - 140;
+        this.splitY[i] = this.y;
+        this.splitSpeed[i] = random(1, 3);
+      }
+    }
+
+    if (this.breakApart) {
+      for (let i = 0; i < this.splitText.length; i++) {
+        this.splitY[i] += this.splitSpeed[i];
+        this.splitSpeed[i] += 0.1;
+      }
+    }
+  }
+  display5() {
+    background(220, 40);
+    textAlign(CENTER);
+
+    textSize(24 + sin(frameCount * 0.05) * 2);
+
+    fill(0, this.final);
 
 
+    if (!this.breakApart) {
+      text("무엇일까요?", this.x, this.y);
+    }
 
-// display1() {
-//     textAlign(CENTER);
-//     fill(0);
-//     text(myInput.value(), this.x, this.y);
-
-//     for (let i = -40; i <= 40; i += 10) {
-//       fill(0);
-//       text(chars[floor(random(chars.length))], this.x + i, this.yU + random(-50, 0));
-//     }
-//   }
+    else {
+      for (let i = 0; i < this.splitText.length; i++) {
+        text(this.splitText[i], this.splitX[i], this.splitY[i]);
+      }
+    }
+  }
+} 
